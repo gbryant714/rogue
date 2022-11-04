2a9f21c95e1b54f51f178de3c92fb0249f88f2f9
Grace Bryant <95504446+gbryant714@users.noreply.github.com>
2022-11-04 10:34:08 -0400

    Add files via upload

---
+++ Rogue/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Figgle;
+
+/*Stop making lame round trips from Wowhead to Wowsims to IcyVeins and back...
+ * Compiling and keeping all rogue information here for your convenience!
+ * Request bis list, optional bis by slot, stat priority, hit cap, exp cap... */
+
+string greeting = "Rogue";
+
+//hide all this junk in config/json file 
+Console.WriteLine(FiggleFonts.Slant.Render(greeting));
+Console.WriteLine("Make your request... \r\n" +
+                  "1. BIS List by Spec \r\n" +
+                  "2. BIS for an item slot \r\n" +
+                  "3. Stat Priority for my spec \r\n" +
+                  "4. My last parse for boss X (WiP)");
